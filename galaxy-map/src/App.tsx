b53474cc7cc53d3656d0327dda91ed9082eac43b
Zach Bagley <szachbagley@gmail.com>
2026-04-14 22:17:49 -0600

    grid testing

--- galaxy-map/src/App.tsx
+++ galaxy-map/src/App.tsx
@@ -1,22 +1,13 @@
 import { useAppContext } from './context/AppContext.tsx';
 import './App.css'
+import { Grid } from './components/Grid.tsx';
 
 function App() {
-  const { state, dispatch } = useAppContext();
+  // const { state, dispatch } = useAppContext();
 
   return (
     <>
-      <section id="center">
-        <h1>Systems:</h1>
-        <ul>
-          {state.systems.map((system) => (
-            <li key={system.id}>{system.name}</li>
-          ))}
-        </ul>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Grid />
     </>
   )
 }
